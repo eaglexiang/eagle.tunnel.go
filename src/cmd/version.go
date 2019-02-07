6a852597cbdbd7565c0b6f69e53dbaaa02fd0101
Eagle <eagle.xiang@outlook.com>
2019-02-07 18:32:09 +0800

    更新版本号

--- src/cmd/version.go
+++ src/cmd/version.go
@@ -4,7 +4,7 @@
  * @Github: https://github.com/eaglexiang
  * @Date: 2018-12-27 09:42:11
  * @LastEditors: EagleXiang
- * @LastEditTime: 2019-01-21 17:40:39
+ * @LastEditTime: 2019-02-07 18:31:59
  */
 
 package cmd
@@ -16,7 +16,7 @@ import (
 )
 
 // ProgramVersion 程序版本
-var ProgramVersion, _ = myversion.CreateVersion("0.8")
+var ProgramVersion, _ = myversion.CreateVersion("0.8.1")
 
 // PrintVersion 打印版本信息
 func PrintVersion(programVersion, protocolVersion, ProtocolCompatibleVersion string) {
