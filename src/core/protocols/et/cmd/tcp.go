4f6ba8152210a1c5a68d62e9b2c2c8f5b3a9ce3b
Eagle <eagle.xiang@outlook.com>
2019-04-02 21:21:49 +0800

    重构dns解析相关

--- src/core/protocols/et/cmd/tcp.go
+++ src/core/protocols/et/cmd/tcp.go
@@ -3,7 +3,7 @@
  * @Github: https://github.com/eaglexiang
  * @Date: 2018-12-23 22:54:58
  * @LastEditors: EagleXiang
- * @LastEditTime: 2019-04-01 22:12:46
+ * @LastEditTime: 2019-04-02 21:19:11
  */
 
 package cmd
@@ -18,6 +18,16 @@ import (
 	mynet "github.com/eaglexiang/go-net"
 	mytunnel "github.com/eaglexiang/go-tunnel"
 )
+
+var dnsResolver map[string]func(*comm.NetArg) error
+
+func init() {
+	dnsResolver = make(map[string]func(*comm.NetArg) error)
+	dnsResolver["4"] = resolvDNS4
+	dnsResolver["6"] = resolvDNS6
+	dnsResolver["46"] = resolvDNS46
+	dnsResolver["64"] = resolvDNS64
+}
 
 // TCP ET-TCP子协议的实现
 type TCP struct {
@@ -57,61 +67,67 @@ func (t TCP) Send(e *comm.NetArg) (err error) {
 	return nil
 }
 
-func (t TCP) resolvDNS(e *comm.NetArg) (err error) {
-	// 调用DNS Sender解析Domain为IP
-	switch comm.ETArg.IPType {
-	case "4":
-		err = comm.SubSenders[comm.DNS].Send(e)
-	case "6":
-		err = comm.SubSenders[comm.DNS6].Send(e)
-	case "46":
-		err = comm.SubSenders[comm.DNS].Send(e)
-		if err != nil {
-			err = comm.SubSenders[comm.DNS6].Send(e)
-		}
-	case "64":
-		err = comm.SubSenders[comm.DNS6].Send(e)
-		if err != nil {
-			err = comm.SubSenders[comm.DNS].Send(e)
-		}
-	default:
-		logger.Warning("invalid ip-type: ", comm.ETArg.IPType)
-		err = errors.New("TCP.Send -> invalid ip-type")
+func resolvDNS4(e *comm.NetArg) error {
+	return comm.SubSenders[comm.DNS].Send(e)
+}
+
+func resolvDNS6(e *comm.NetArg) error {
+	return comm.SubSenders[comm.DNS6].Send(e)
+}
+
+func resolvDNS46(e *comm.NetArg) (err error) {
+	if err = resolvDNS4(e); err != nil {
+		err = resolvDNS6(e)
 	}
-	return err
+	return
+}
+
+func resolvDNS64(e *comm.NetArg) (err error) {
+	if err = resolvDNS6(e); err != nil {
+		err = resolvDNS4(e)
+	}
+	return
+}
+
+func (t TCP) resolvDNS(e *comm.NetArg) error {
+	resolver, ok := dnsResolver[comm.ETArg.IPType]
+	if !ok {
+		logger.Error("invalid ip-type", comm.ETArg.IPType)
+		return errors.New("invalid ip-type")
+	}
+	return resolver(e)
 }
 
 func (t *TCP) smartSend(e *comm.NetArg) (err error) {
 	switch e.DomainType {
 	case comm.DirectDomain:
 		logger.Info("connect direct domain ", e.Domain)
-		err = t.sendTCPReq2Server(e)
+		err = t.connectByLocal(e)
 	case comm.ProxyDomain:
 		logger.Info("connect proxy domain ", e.Domain)
-		err = t.sendTCPReq2Remote(e)
+		err = t.connectByProxy(e)
 	default:
 		logger.Info("connect uncertain domain ", e.Domain)
-		err = t.sendTCPReqByLocation(e)
+		err = t.connectByLocation(e)
 	}
 	return err
 }
 
-func (t TCP) sendTCPReqByLocation(e *comm.NetArg) (err error) {
-	l := comm.SubSenders[comm.LOCATION]
-	err = l.Send(e)
+func (t TCP) connectByLocation(e *comm.NetArg) (err error) {
+	err = comm.SubSenders[comm.LOCATION].Send(e)
 	if err != nil {
 		return err
 	}
 	if checkProxyByLocation(e.Location) {
-		err = t.sendTCPReq2Remote(e)
+		err = t.connectByProxy(e)
 	} else {
-		err = t.sendTCPReq2Server(e)
+		err = t.connectByLocal(e)
 	}
 	return err
 }
 
 func (t TCP) proxySend(e *comm.NetArg) error {
-	return t.sendTCPReq2Remote(e)
+	return t.connectByProxy(e)
 }
 
 // Type ET子协议的类型
@@ -124,7 +140,7 @@ func (t TCP) Name() string {
 	return comm.TCPTxt
 }
 
-func (t *TCP) sendTCPReq2Remote(e *comm.NetArg) error {
+func (t *TCP) connectByProxy(e *comm.NetArg) error {
 	logger.Info("connect ", e.IP, " by proxy")
 	err := comm.Connect2Remote(e.Tunnel)
 	if err != nil {
@@ -146,9 +162,7 @@ func (t *TCP) sendTCPReq2Remote(e *comm.NetArg) error {
 	return err
 }
 
-func (t *TCP) sendTCPReq2Server(e *comm.NetArg) error {
-	logger.Info("connect ", e.IP, " by local")
-
+func (t *TCP) connectByLocal(e *comm.NetArg) error {
 	if e.IP == "0.0.0.0" || e.IP == "::" {
 		logger.Info("invalid ip: ", e.IP)
 		return errors.New("invalid ip")
@@ -160,6 +174,7 @@ func (t *TCP) sendTCPReq2Server(e *comm.NetArg) error {
 	} else {
 		ipe = "[" + e.IP + "]:" + e.Port // [ipv6]:port
 	}
+	logger.Info("connect ", ipe, " by local")
 	conn, err := net.DialTimeout("tcp", ipe, comm.Timeout)
 	if err != nil {
 		logger.Warning(err)
@@ -180,14 +195,14 @@ func (t TCP) Handle(req string, tunnel *mytunnel.Tunnel) error {
 	}
 	ip := reqs[1]
 	port := reqs[2]
-	e := comm.NetArg{
+	e := &comm.NetArg{
 		NetConnArg: comm.NetConnArg{
 			IP:   ip,
 			Port: port,
 		},
 		Tunnel: tunnel,
 	}
-	err := t.sendTCPReq2Server(&e)
+	err := t.connectByLocal(e)
 	if err != nil {
 		tunnel.WriteLeft([]byte(err.Error()))
 		return err
